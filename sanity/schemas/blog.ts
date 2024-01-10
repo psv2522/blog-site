0eda947061a9de526296cab3efd33f95c58fd446
psv2522 <vhatkarps20.comp@coep.ac.in>
2024-01-10 09:48:36 +0530

    Sanity schema,4 articles published and starter nextjs

---
+++ sanity/schemas/blog.ts
@@ -0,0 +1,37 @@
+export default {
+  name: "blog",
+  type: "document",
+  title: "Blog",
+  fields: [{
+    name: "title",
+    type: "string",
+    title: "title of blog article",
+  },
+  {
+    name: "slug",
+    type: "slug",
+    title: "Slug of your blog article",
+    options: {
+      source:"title" 
+    }
+  }, {
+    name: "titleImage",
+    type: "image",
+    title: "Title Image"
+  }, {
+    name: "smallDescription",
+    type: "text",
+    title: "Small Description"
+  },
+  {
+    name: "content",
+    type: "array",
+    title: "content",
+    of: [
+      {
+        type: "block",
+      }
+    ]
+  }
+  ],
+}
